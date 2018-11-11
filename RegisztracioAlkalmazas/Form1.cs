df2a74ad9371d9852fba7a0ee78d28d788618e02
weiszapollo <weiszapollo@gmail.com>
2018-11-11 21:00:08 +0100

    vasarnap

--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RegisztracioAlkalmazas
 {
@@ -15,15 +16,16 @@ namespace RegisztracioAlkalmazas
         public Form1()
         {
             InitializeComponent();
-            string name;
-            DateTime szul;
-            string nem;
+
             string hobbi;
-            
+            string nem="";
 
             listBox1.Items.Add("Uszas");
             listBox1.Items.Add("Horgaszat");
             listBox1.Items.Add("Futas");
+
+            
+            listBox1.SetSelected(0, true);
 
             button1.Click += (sender, e) =>
               {
@@ -33,30 +35,66 @@ namespace RegisztracioAlkalmazas
               };
 
             button2.Click += (sender, e) =>
-              {
-                  if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                  bool go = false; 
+
+                if (string.IsNullOrWhiteSpace(textBox1.Text) || (dateTimePicker1.Value.Day >= DateTime.Now.Day) || (!(radioButton1.Checked) && !(radioButton2.Checked)))
+                {
+                    go = false;
+                }
+                else
+                {
+                    if (radioButton1.Checked)
+                    {
+                        nem = "Ferfi";
+                    }
+                    else
+                    {
+                        nem = "No";
+                    }
+                    go = true;
+                }
+                  if (go)
                   {
-                      name = textBox1.Text;
-                      if (dateTimePicker1.Value != DateTime.Now)
+                    string outText = textBox1.Text+" "+dateTimePicker1.Value.ToShortDateString() +" " + nem + " " + listBox1.GetItemText(listBox1.SelectedItem);
+                    using (System.IO.StreamWriter file= new System.IO.StreamWriter(@"C: \Users\user\source\repos\RegisztracioAlkalmazas\adatok.txt"))
+                    {
+
+                        file.WriteLine(outText);                         
+                    }
+                    textBox1.Text = "";
+                    dateTimePicker1.Value = DateTime.Now;
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+
+                    MessageBox.Show("Sikeres bevitel");
+                  }
+                  else
+                  {
+                      MessageBox.Show("Nem töltötted ki a formot rendese");
+                  }
+              
+            };
+
+            button3.Click += (sender, e) =>
+              {
+                  using (StreamReader sr = new StreamReader(@"C: \Users\user\source\repos\RegisztracioAlkalmazas\adatok.txt"))
+                  {
+                      String line = sr.ReadToEnd();
+                      String[] array = line.Split();
+                      textBox1.Text = array[0];
+                      dateTimePicker1.Value = DateTime.Parse(array[1]);
+                      if (array[2] == "Ferfi")
                       {
-                          szul = dateTimePicker1.Value;
-                          if(radioButton1.Checked || radioButton2.Checked)
-                          {
-                              if (radioButton1.Checked)
-                              {
-                                  nem = "Ferfi";
-                              }
-                              nem = "No";
-                              if (listBox1.SelectedIndex == -1)
-                              {
-                                  Console.WriteLine("Nem valasztottal hobbit");
-                              }
-                              else
-                              {
-                                  hobbi = listBox1.GetItemText(listBox1.SelectedItem);
-                              }
-                          }
+                          radioButton1.Checked = true;
                       }
+                      else
+                      {
+                          radioButton2.Checked = true;
+                      }
+
+                      
+
                   }
               };
 
